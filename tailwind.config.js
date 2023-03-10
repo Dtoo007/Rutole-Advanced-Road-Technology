4b5455d168cd43af1ee888a27e6066c52c51c56c
Abbas Oladimeji <taiwoabbas007@gmail.com>
2023-03-10 04:55:10 +0100

    file updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    
+    container: {
+      center: (true),
+      padding: {
+        md:'6rem',
+      sm: '0rem',
+      lg: '4rem',
+      xl: '5rem',
+    },
+ },
+    extend: {
+      colors: {
+        off_white: '#F2F2F2',
+        off_black: '#110F0F',
+        off_arch: '#6B6A6A'
+      },
+
+      fontFamily: {
+        'sans': ['Bubblegum Sans'],
+        'hind': ['hind']
+      },
+    },
+
+    plugins: [
+      require('@tailwindcss/typography'),
+    ]
   },
-  plugins: [],
+  
+  plugins: [function({addBase, theme}) {
+    addBase({
+      '@import': [
+        'url("https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap")',
+        'url("https://fonts.googleapis.com/css2?family=Hind:wght@400;500&display=swap")',
+      ]
+    })
+  }],
 }
